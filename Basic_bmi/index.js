18397627c285c39904d592c65eaba31e86240d67
ULADZIMIR MARTYNCHYK <01myit@gmail.com>
2024-03-25 19:36:42 +0300

    BMI. Used Math.pow(). Added preventDefault method.

--- Basic_bmi/index.js
+++ Basic_bmi/index.js
@@ -1,15 +1,16 @@
 const btn = document.querySelector('.bmiForm .btn')
-btn.addEventListener('click', createBmiField)
+btn.addEventListener('click', (e) => createBmiField(e))
 function calcBmi() {
   const height = document.querySelector('.bmiForm .height').value
   const weight = document.querySelector('.bmiForm .weight').value
-  const bmi = weight / (height * height)
+  const bmi = weight / Math.pow(height / 100, 2)
   return bmi
 }
 
-function createBmiField() {
+function createBmiField(e) {
+  e.preventDefault()
   const bmi = calcBmi().toFixed(2)
-  const bmiResult = bmi > 25 ? `${bmi} over weight!!!` : `${bmi} normal weight`
+  const bmiResult = bmi >= 25 ? `${bmi} over weight!!!` : `${bmi} normal weight`
   const div = document.createElement('div')
   div.classList.add('indexBmi')
   div.textContent = bmiResult
