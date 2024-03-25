c44b8c74daec7d46a1903d44971dd76c7086ef44
ULADZIMIR MARTYNCHYK <01myit@gmail.com>
2024-03-25 21:16:40 +0300

    Now you can see your overweight, if it is.

--- Basic_bmi/index.js
+++ Basic_bmi/index.js
@@ -3,16 +3,21 @@ btn.addEventListener('click', (e) => createBmiField(e))
 function calcBmi() {
   const height = document.querySelector('.bmiForm .height').value
   const weight = document.querySelector('.bmiForm .weight').value
-  const bmi = weight / Math.pow(height / 100, 2)
-  return bmi
+  const bmi = (weight / Math.pow(height / 100, 2)).toFixed(2)
+  const overBmi = bmi - 25
+  const overWeight = overBmi > 0 ? Math.round(overBmi * Math.pow(height / 100, 2)) : ''
+  return { bmi, overWeight }
 }
 
 function createBmiField(e) {
   e.preventDefault()
-  const bmi = calcBmi().toFixed(2)
-  const bmiResult = bmi >= 25 ? `${bmi} over weight!!!` : `${bmi} normal weight`
+  const { bmi, overWeight } = calcBmi()
+  const bmiResult =
+    bmi >= 25
+      ? `<p>Your BMI is ${bmi}.</p> <p>Your overweight is near ${overWeight}!!!</p>`
+      : `${bmi} Your weight is normal!`
   const div = document.createElement('div')
   div.classList.add('indexBmi')
-  div.textContent = bmiResult
+  div.innerHTML = bmiResult
   document.querySelector('.bmiForm').append(div)
 }
