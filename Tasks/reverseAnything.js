f07bb995fbeb0f89cbf0d6a52055ce2b144fd811
ULADZIMIR MARTYNCHYK <01myit@gmail.com>
2024-04-01 20:27:52 +0300

    Now the function can use WeakMap and WeakSet.

--- Tasks/reverseAnything.js
+++ Tasks/reverseAnything.js
@@ -4,6 +4,7 @@ function reverseAll(data) {
   if (typeof data === 'string') return data.split('').toReversed().join('')
   if (data.constructor.name === 'Set') return new Set(Array.from(data).toReversed())
   if (data.constructor.name === 'Object' || data.constructor.name === 'Map') return { ...data }
+  if (data.constructor.name === 'WeakMap' || data.constructor.name === 'WeakSet') return data
   if (typeof data === 'number') return Number(data.toString().split('').toReversed().join(''))
 }
 const abc = 'abc'
@@ -13,6 +14,14 @@ console.log(reverseAll([1, 2, 3]))
 console.log(reverseAll(num))
 console.log(reverseAll(new Set([1, 2, 3, 'set'])))
 console.log(reverseAll({ a: 1 }))
+const ws = new WeakSet()
+ws.add({ a: 1, b: 2, c: 3 })
+console.log(reverseAll(ws), 'WeakSet')
+const wm = new WeakMap()
+wm.set({}, 1)
+wm.set({}, 2)
+wm.set({}, 3)
+console.log(reverseAll(wm), 'WeakMap')
 console.log('----------------------')
 console.log('prev', abc)
 console.log('prev', num)
