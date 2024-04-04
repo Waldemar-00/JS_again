9cce42b7964eb723819fd04351f8c11a4241e150
ULADZIMIR MARTYNCHYK <01myit@gmail.com>
2024-04-04 20:52:27 +0300

    Code and bugs were fixed. Code refactoring.

--- Games/Guess number/index.js
+++ Games/Guess number/index.js
@@ -5,36 +5,41 @@ const input = document.querySelector("[type = 'number']")
 const gameMessage = document.querySelector('#game-message')
 const pointsNumber = document.querySelector('#points span')
 const bestResultNumber = document.querySelector('#best-result span')
-
+let prevValue = ''
 function randomNum() {
   return Math.floor(Math.random() * 20 + 1)
 }
 const gameNumber = randomNum()
 
 function startNewGame() {
+  checkOut.addEventListener('click', checkNumber)
   h2.innerText = '???'
   input.value = ''
+  prevValue = ''
+  bestResultNumber.innerText = `${pointsNumber.innerText}`
   gameMessage.innerText = 'Start guessing!'
   pointsNumber.innerText = '20'
-  bestResult.innerText = `Best Result: ${pointsNumber.innerText}`
   return randomNum()
 }
+
 function checkNumber() {
-  const numberFromInput = Number(input.value)
-  if (numberFromInput > gameNumber) {
+  if (prevValue == input.value) return
+  prevValue = input.value
+  if (input.value > gameNumber) {
     gameMessage.innerText = 'Too much!'
     pointsNumber.innerText = pointsNumber.innerText - 1
   }
-  if (numberFromInput < gameNumber) {
+  if (input.value < gameNumber) {
     gameMessage.innerText = 'Too few!'
     pointsNumber.innerText = pointsNumber.innerText - 1
   }
-  if (numberFromInput === gameNumber) {
+  if (input.value == gameNumber) {
     gameMessage.innerText = 'YOU WIN!'
     pointsNumber.innerText = pointsNumber.innerText - 1
     bestResultNumber.innerText = pointsNumber.innerText
     h2.classList.add('win')
     h2.innerText = gameNumber
+    checkOut.removeEventListener('click', checkNumber)
   }
 }
 atFirst.addEventListener('click', startNewGame)
