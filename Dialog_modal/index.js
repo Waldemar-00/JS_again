f311b024ab3a4357be3ea24f288fa24ad84127c0
ULADZIMIR MARTYNCHYK <01myit@gmail.com>
2024-04-07 18:27:40 +0300

    Data assembly.

---
+++ Dialog_modal/index.js
@@ -0,0 +1,15 @@
+const btnGet = document.querySelector('.get')
+const btnClose = document.querySelector('.close')
+const inputName = document.querySelector('[name="name"]')
+const inputSourname = document.querySelector('[name="sourname"]')
+const inputAge = document.querySelector('[name="age"]')
+const form = document.querySelector('.modalForm')
+const btnForm = document.querySelector('.modalForm button')
+const dialog = document.querySelector('.modal')
+
+btnGet.addEventListener('click', () => dialog.showModal())
+btnClose.addEventListener('click', () => dialog.close())
+form.addEventListener('submit', (e) => {
+  e.preventDefault()
+  console.log({ name: inputName.value, sourname: inputSourname.value, age: inputAge.value })
+})
